3891c775c5362bb568e882d5687239b5b59c42aa
FlakeSnows <andreydfilippov@gmail.com>
2026-06-10 10:39:55 +0500

    1201B

--- greedy/1201B.py
+++ greedy/1201B.py
@@ -1,2 +1,11 @@
 # B.Обнуление массива
+n = int(input())
+a = list(map(int,input().split()))
 
+if sum(a) % 2 == 0:
+    if max(a) <= sum(a) - max(a):
+        print('YES')
+    else:
+        print('NO')
+else:
+    print('NO')
